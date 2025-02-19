102b885029a78cf31cf21fcaa9e13c865bb21077
Serge Thomas <smughemuzi@gmail.com>
2025-02-19 13:44:18 +0200

    config files

--- personalWebsite/settings.py
+++ personalWebsite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -48,7 +48,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this for static file serving
 ]
+
+# CORS Setup (If using APIs)
+# INSTALLED_APPS += ['corsheaders']
+# MIDDLEWARE.insert(1, 'corsheaders.middleware.CorsMiddleware')
+
+# CORS_ALLOWED_ORIGINS = ["https://your-app.up.railway.app"]
 
 ROOT_URLCONF = 'personalWebsite.urls'
 
@@ -116,7 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static Files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
