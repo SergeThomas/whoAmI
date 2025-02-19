e9b0df2091ea84068585295a29ed9684dad28492
Serge Thomas <smughemuzi@gmail.com>
2025-02-19 22:47:11 +0200

    adding superuser build

--- build.sh
+++ build.sh
@@ -6,8 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
-
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input
-fi
+python manage.py createsuperuser --no-input
